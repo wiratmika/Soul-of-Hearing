39a4949ba553e929975d23ba573a1b9dbec0ce3b
Wiratmika <wiratmika@gmail.com>
2015-12-24 22:21:36 +0700

    Migrate to smoothscroll.js, modify .gitignore to include Foundation's scaffolding files

--- gulpfile.js
+++ gulpfile.js
@@ -57,6 +57,7 @@ var PATHS = {
     'bower_components/foundation-sites/js/foundation.toggler.js',
     'bower_components/foundation-sites/js/foundation.tooltip.js',
     'bower_components/foundation-datepicker/js/foundation-datepicker.js',
+    'bower_components/smooth-scroll.js/dist/js/smooth-scroll.js',
     'src/assets/js/**/*.js',
     'src/assets/js/app.js'
   ]
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,19 +1,3 @@
 $(document).foundation();
 $('#birthdate').fdatepicker();
-
-$(function smoothScroll() {
-  'use strict';
-
-  $('a[href*=#]:not([href=#])').click(function clickListener() {
-    if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
-      let target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top,
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
+smoothScroll.init();
