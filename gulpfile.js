a93d4ee5a74ad8976162d53fd276afbda386be4b
Wiratmika <wiratmika@gmail.com>
2015-12-25 20:46:32 +0700

    add

--- gulpfile.js
+++ gulpfile.js
@@ -37,25 +37,25 @@ var PATHS = {
     'bower_components/foundation-sites/js/foundation.core.js',
     'bower_components/foundation-sites/js/foundation.util.*.js',
     // Paths to individual JS components defined below
-    'bower_components/foundation-sites/js/foundation.abide.js',
-    'bower_components/foundation-sites/js/foundation.accordion.js',
-    'bower_components/foundation-sites/js/foundation.accordionMenu.js',
-    'bower_components/foundation-sites/js/foundation.drilldown.js',
+    // 'bower_components/foundation-sites/js/foundation.abide.js',
+    // 'bower_components/foundation-sites/js/foundation.accordion.js',
+    // 'bower_components/foundation-sites/js/foundation.accordionMenu.js',
+    // 'bower_components/foundation-sites/js/foundation.drilldown.js',
     'bower_components/foundation-sites/js/foundation.dropdown.js',
     'bower_components/foundation-sites/js/foundation.dropdownMenu.js',
-    'bower_components/foundation-sites/js/foundation.equalizer.js',
-    'bower_components/foundation-sites/js/foundation.interchange.js',
-    'bower_components/foundation-sites/js/foundation.magellan.js',
-    'bower_components/foundation-sites/js/foundation.offcanvas.js',
+    // 'bower_components/foundation-sites/js/foundation.equalizer.js',
+    // 'bower_components/foundation-sites/js/foundation.interchange.js',
+    // 'bower_components/foundation-sites/js/foundation.magellan.js',
+    // 'bower_components/foundation-sites/js/foundation.offcanvas.js',
     'bower_components/foundation-sites/js/foundation.orbit.js',
     'bower_components/foundation-sites/js/foundation.responsiveMenu.js',
     'bower_components/foundation-sites/js/foundation.responsiveToggle.js',
-    'bower_components/foundation-sites/js/foundation.reveal.js',
-    'bower_components/foundation-sites/js/foundation.slider.js',
-    'bower_components/foundation-sites/js/foundation.sticky.js',
-    'bower_components/foundation-sites/js/foundation.tabs.js',
+    // 'bower_components/foundation-sites/js/foundation.reveal.js',
+    // 'bower_components/foundation-sites/js/foundation.slider.js',
+    // 'bower_components/foundation-sites/js/foundation.sticky.js',
+    // 'bower_components/foundation-sites/js/foundation.tabs.js',
     'bower_components/foundation-sites/js/foundation.toggler.js',
-    'bower_components/foundation-sites/js/foundation.tooltip.js',
+    // 'bower_components/foundation-sites/js/foundation.tooltip.js',
     'bower_components/foundation-datepicker/js/foundation-datepicker.js',
     'bower_components/smooth-scroll.js/dist/js/smooth-scroll.js',
     'src/assets/js/**/*.js',
@@ -130,7 +130,7 @@ gulp.task('sass', function() {
     .pipe($.autoprefixer({
       browsers: COMPATIBILITY
     }))
-    .pipe(uncss)
+    // .pipe(uncss)
     .pipe(minifycss)
     .pipe($.if(!isProduction, $.sourcemaps.write()))
     .pipe(gulp.dest('dist/assets/css'));
